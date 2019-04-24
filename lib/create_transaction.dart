b431d6d24c95b8396bdb8768b6978f64932cd854
Brendan Arciszewski <git@brendanarciszewski.ca>
2019-04-24 11:12:57 -0400

    income/expense slider

--- lib/create_transaction.dart
+++ lib/create_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'data/data.dart';
 
 class CreateTransactionForm extends StatefulWidget {
@@ -9,9 +10,13 @@ class CreateTransactionForm extends StatefulWidget {
 
 class _CreateTransactionFormState extends State<CreateTransactionForm> {
   final _formKey = new GlobalKey<FormState>();
+
   Category _category;
   Category _subcategory;
-  DateTime _time;
+  DateTime _date_time;
+  double _amount, _sign = 0.0;
+  String _vendor;
+
   Future<List<Category>> _storedCategoriesFuture;
 
   @override
@@ -38,26 +43,88 @@ class _CreateTransactionFormState extends State<CreateTransactionForm> {
           case ConnectionState.done:
             if (snapshot.hasError)
               return new Text(snapshot.error);
-
-            final categories = snapshot.data;
-
             return new Form(
               key: this._formKey,
               child: new Column(
                 children: <Widget>[
+                  new Row(
+                    children: <Widget>[
+                      const Spacer(),
+                      const Text('Expense'),
+                      const Spacer(),
+                      new SliderTheme(
+                        data: new SliderThemeData(
+                          trackHeight: 5,
+                          activeTrackColor: Colors.green[700],
+                          inactiveTrackColor: Colors.red[600],
+                          disabledActiveTrackColor: Colors.grey,
+                          disabledInactiveTrackColor: Colors.grey,
+                          activeTickMarkColor: Colors.green[700],
+                          inactiveTickMarkColor: Colors.red[600],
+                          disabledActiveTickMarkColor: Colors.grey,
+                          disabledInactiveTickMarkColor: Colors.grey,
+                          thumbColor: Colors.black,
+                          disabledThumbColor: Colors.grey,
+                          overlayColor: Colors.grey,
+                          valueIndicatorColor: Colors.grey,
+                          trackShape: RectangularSliderTrackShape(),
+                          tickMarkShape: RoundSliderTickMarkShape(),
+                          thumbShape: RoundSliderThumbShape(),
+                          overlayShape: RoundSliderOverlayShape(),
+                          valueIndicatorShape: PaddleSliderValueIndicatorShape(),
+                          showValueIndicator: ShowValueIndicator.never,
+                          valueIndicatorTextStyle: TextStyle(),
+                        ),
+                        child: new Slider(
+                          onChanged: (double value) {
+                            setState(() {
+                              this._sign = value;
+                            });
+                          },
+                          min: -1.0,
+                          max: 1.0,
+                          divisions: 1,
+                          value: this._sign,
+                      )),
+                      const Spacer(),
+                      const Text('Income'),
+                      const Spacer(),
+                    ]
+                  ),
                   new TextFormField(
-                    validator: (value) {
+                    validator: (String value) {
                       if (value.isEmpty) return 'Enter cost!';
+                      if (double.tryParse(value) == null) return 'Enter cost!';
+                    },
+                    onSaved: (String value) {
+                      setState(() {
+                        this._amount = double.parse(value);
+                      });
                     },
                     keyboardType: TextInputType.numberWithOptions(
-                        signed: true, decimal: true),
+                        signed: false, decimal: true),
                     decoration: new InputDecoration(
                       icon: new Icon(Icons.attach_money),
                       hintText: 'Enter the Total',
                     ),
                   ),
+                  new TextFormField(
+                    validator: (String value) {
+                      if (value.isEmpty) return 'Enter vendor!';
+                    },
+                    onSaved: (String value) {
+                      setState(() {
+                        this._vendor = value;
+                      });
+                    },
+                    keyboardType: TextInputType.text,
+                    decoration: new InputDecoration(
+                      icon: new Icon(Icons.business),
+                      hintText: "Enter the Vendor's Name",
+                    ),
+                  ),
                   new DropdownButton<Category>(
-                    items: listToDropdownList(categories),
+                    items: listToDropdownList(snapshot.data),
                     value: this._category,
                     hint: const Text('Choose a Category'),
                     onChanged: (Category category) {
@@ -91,8 +158,10 @@ class _CreateTransactionFormState extends State<CreateTransactionForm> {
                           context: context,
                           initialTime: TimeOfDay.now()
                       );
-                      _time = DateTime(date.year, date.month, date.day,
-                          time.hour, time.minute);
+                      setState(() {
+                        this._date_time = DateTime(date.year, date.month, date.day,
+                            time.hour, time.minute);
+                      });
                     },
                   ),
                 ],
